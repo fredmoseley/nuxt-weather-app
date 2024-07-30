beac6575a3e5cb112fce7e136ea6df349920adb9
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-30 11:53:16 -0700

    Find the long and lat with the geocode API

--- server/api/openweathermap.js
+++ server/api/openweathermap.js
@@ -1,14 +1,16 @@
 export default defineEventHandler(async (event) => {
   const { zipCode } = getQuery(event);
-  console.log('openweathermap.js: API Server getQuery zipCode:', zipCode);
-  const { lat, lon } = {
-    lat: 37.696255,
-    lon: -122.049904,
-  };
   const { weatherAPIKey } = useRuntimeConfig();
-  const uri = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
 
-  const data = await $fetch(uri);
-
-  return data;
+  console.log('openweathermap.js: API Server getQuery zipCode:', zipCode);
+  try {
+    const locationUri = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${weatherAPIKey}`;
+    const { name, lat, lon, country } = await $fetch(locationUri);
+    const weatherUri = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
+    console.log({ name, country, lat, lon }, weatherUri);
+    const data = await $fetch(weatherUri);
+    return data;
+  } catch (error) {
+    console.error(`Error looking up weather: ${error}`);
+  }
 });
