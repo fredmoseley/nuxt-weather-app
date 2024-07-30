c3e29bb496e61aa36a1c23010f381052161e4600
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-29 19:05:07 -0700

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+<template>
+  <h1>About.html</h1>
+  <div></div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<script setup>
+const zipCode = useState('zipCode', () => '');
+const zipCodeRegex = /^\d{5}$/;
+
+
+const onSubmit = () => {
+  const zipCodeInput = document.getElementById('zipCodeInput');
+  console.log('Log the zipCode:', zipCode.value);
+  if (zipCodeRegex.test(zipCode.value)) {
+    console.log('Valid ZipCode');
+  } else {
+    console.log('Invalid ZipCode');
+  }
+};
+</script>
+
+<template>
+    <div class="container max-w-sm mx-auto py-6">
+      <form @submit.prevent="onSubmit">
+        <label
+          for="zipCode"
+          class="block text-gray-700 font-bold mb-2"
+          >ZIP Code</label
+        >
+        <input
+          id="zipCodeInput"
+          type="text"
+          minlength="5"
+          maxlength="5"
+          pattern="\d{5}"
+          name="zip_code"
+          placeholder="Enter 5-digit ZIP code for 5 day weather forecast:"
+          class="border rounded w-full py-2 px-3 mb-2"
+          required
+          v-model="zipCode"
+        />
+        <button
+          type="submit"
+          class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </template>
+<style scoped></style>
