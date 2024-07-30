b990ef28f93e5f6e7285ab8166796ee37d54acd2
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-29 22:37:04 -0700

    Add call to API server to the weather route

--- pages/weather/[zipCode].vue
+++ pages/weather/[zipCode].vue
@@ -1,8 +1,13 @@
 <script setup>
+// 1. Server will make the call api/openweathermap/${routeURL}
+const { zipCode } = useRoute().params;
+console.log('useRoute.params zipCode:', zipCode)
+const { data } = await useFetch(`/api/openweathermap/${zipCode}`)
 
 </script>
 <template>
   <h1 class="">Weather Route</h1>
+  <p>{{ data }}</p>
 </template>
 
 <style scoped></style>
