5d1eca5ac5b5e8f88d76d14fb763dcd24cd337b6
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 14:41:54 -0700

    Clean up code

--- pages/weather/[zipCode].vue
+++ pages/weather/[zipCode].vue
@@ -6,19 +6,19 @@ const { data } = await useFetch(`/api/openweathermap?zipCode=${zipCode}`);
 const { list: forecasts, city } = data.value;
 </script>
 <template>
-  <div class="my-16">
-    <h1 class="text-7xl text-center mb-4">{{ city.name }}</h1>
-    <h1 class="text-5xl text-center">3-Hourly Forecast for the Next 5 Days</h1>
-  </div>
-  <div class="overflow-x-auto sm:overflow-x-visible">
-    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-5 min-w-max">
-      <div
-        v-for="forecast in data.list"
-        :key="forecast.dt"
-      >
-        <WeatherCard
-          :forecast="forecast"
-        />
+  <div>
+    <div class="my-16">
+      <h1 class="text-7xl text-center mb-4">{{ city.name }}</h1>
+      <h1 class="text-5xl text-center">3-Hourly Forecast for the Next 5 Days</h1>
+    </div>
+    <div class="overflow-x-auto sm:overflow-x-visible">
+      <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-5 min-w-max">
+        <div
+          v-for="forecast in forecasts"
+          :key="forecast.dt"
+        >
+          <WeatherCard :forecast="forecast" />
+        </div>
       </div>
     </div>
   </div>
