67f1220f8f7aa881278f334badf3a01759b9b9a8
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-30 17:08:56 -0700

    Clean up API server code

--- server/api/openweathermap.js
+++ server/api/openweathermap.js
@@ -2,14 +2,19 @@ export default defineEventHandler(async (event) => {
   const { zipCode } = getQuery(event);
   const { weatherAPIKey } = useRuntimeConfig();
 
+  const units = 'imperial';
+  const locationUri = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${weatherAPIKey}`;
+
   try {
-    const locationUri = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${weatherAPIKey}`;
+    // Use geocode API to get the lat,lon from zip
     const { name, lat, lon, country } = await $fetch(locationUri);
-    const weatherUri = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
-    console.info({ name, country, lat, lon }, weatherUri);
+
+    // Use openweathermap api to get the forecast
+    const weatherUri = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${weatherAPIKey}`;
     const data = await $fetch(weatherUri);
+
     return data;
   } catch (error) {
-    console.error(`Error looking up weather: ${error}`);
+    console.error(`Error retrieving the weather forecast: ${error}`);
   }
 });
