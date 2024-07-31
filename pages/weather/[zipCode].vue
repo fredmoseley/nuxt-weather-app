d585a408bf4a4aac4bf8dc43e40a4558a834111a
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 14:36:11 -0700

    Make the grid responsive for smaller screens

--- pages/weather/[zipCode].vue
+++ pages/weather/[zipCode].vue
@@ -10,8 +10,8 @@ const { list: forecasts, city } = data.value;
     <h1 class="text-7xl text-center mb-4">{{ city.name }}</h1>
     <h1 class="text-5xl text-center">3-Hourly Forecast for the Next 5 Days</h1>
   </div>
-  <div>
-    <div class="grid grid-cols-8 gap-5">
+  <div class="overflow-x-auto sm:overflow-x-visible">
+    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-5 min-w-max">
       <div
         v-for="forecast in data.list"
         :key="forecast.dt"
