3eae7a229294e77387c5374f96a3d7c80abd0a78
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 13:39:14 -0700

    Adjust WeatherCard styling

--- components/WeatherCard.vue
+++ components/WeatherCard.vue
@@ -7,13 +7,12 @@ const [{id, main, description, icon}] = weather
 
 <template>
   <div class="card text-center my-6">
-    <p class="font-bold text-gray-500 m-4">{{ dayOfWeek }}</p>
-    <p class="font-bold text-gray-500 m-4">{{ month }} {{ day }}</p>
-    <p class="font-bold text-gray-500 m-4">{{ time }}</p>
+    <p class="m-4">{{ dayOfWeek }} {{ month }} {{ day }} {{ year }}</p>
+    <p class="m-4">{{ time }}</p>
     <img :src="`https://openweathermap.org/img/wn/${icon}@2x.png`" alt="weather icon" class="mx-auto">
     <p class="font-bold text-gray-500 m-4">{{ Math.round(temperature.temp) }}°</p>
     <p class="font-bold text-gray-500 m-4">{{ main }}</p>
-    <p class="font-bold text-gray-500 m-4">{{ description }}</p>
+    <p class=" text-gray-500 m-4">{{ description }}</p>
   </div>
 </template>
 
