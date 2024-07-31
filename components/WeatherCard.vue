ef82238c34ce0407266d79b7094730e1dfee9871
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-30 17:10:04 -0700

    Display the weather forecast

---
+++ components/WeatherCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { forecast } = defineProps(['forecast'])
+const {main: temperature, weather, dt_txt } = forecast
+const [dayOfWeek, month, day, year, time] = new Date(dt_txt).toString().split(' ');
+const [{id, main, description, icon}] = weather
+</script>
+
+<template>
+  <div class="card text-center">
+    <p class="font-bold text-gray-500 m-4">{{ dayOfWeek }}</p>
+    <p class="font-bold text-gray-500 m-4">{{ month }} {{ day }}</p>
+    <p class="font-bold text-gray-500 m-4">{{ time }}</p>
+    <img :src="`https://openweathermap.org/img/wn/${icon}@2x.png`" alt="weather icon" class="mx-auto">
+    <p class="font-bold text-gray-500 m-4">{{ Math.round(temperature.temp) }}°</p>
+    <p class="font-bold text-gray-500 m-4">{{ main }}</p>
+    <p class="font-bold text-gray-500 m-4">{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- pages/weather/[zipCode].vue
+++ pages/weather/[zipCode].vue
@@ -1,14 +1,24 @@
 <script setup>
-// 1. Server will make the call api/openweathermap/${routeURL}
 const { zipCode } = useRoute().params;
 console.log('[zipCode].vue useRoute.params zipCode:', zipCode)
-// const { data } = await useFetch(`/api/openweathermap/${zipCode}`)
 const { data } = await useFetch(`/api/openweathermap?zipCode=${zipCode}`)
+
+const {list: forecasts, city} = data.value
 
 </script>
 <template>
-  <h1 class="">Weather Route</h1>
-  <p>{{ data }}</p>
+  <h1 class="text-7xl text-center mt-24 mb-8">{{ city.name }}</h1>
+  <h1 class="text-5xl text-center mb-24 ">3-Hourly Forecast for the Next 5 Days</h1>
+  <div>
+    <div class="grid grid-cols-8 gap-5">
+      <div
+        v-for="forecast in data.list"
+        :key="forecast.dt"
+      >
+        <WeatherCard :forecast="forecast" class="my-12" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
