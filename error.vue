03271604a6606ccb4b373f4f76c9a4a99096f02f
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 11:51:00 -0700

    Modify custom error page styling

--- error.vue
+++ error.vue
@@ -4,11 +4,11 @@ defineProps(['error'])
 const handleClearError= () => clearError({redirect:'/'})
 </script>
 <template>
-  <div class="mt-7 max-w-sm mx-auto text-center card">
+  <div class="mt-7 mx-auto text-center card">
     <p class="mt-7 text-7xl font-bold"> {{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Ooops.</p>
-    <p class="mt-7">{{ error.message }}</p>
-    <button class="btn my-7" @click="handleClearError">Go Home....</button>
+    <p class="mt-7 text-3xl">{{ error.message }}</p>
+    <button class="btn my-7" @click="handleClearError">Go Home</button>
   </div>
 </template>
 
