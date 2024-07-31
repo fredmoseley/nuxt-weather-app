c89e175cba3584ffea5eef6f2e91570ea548a94f
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 14:51:43 -0700

    Update server variable names

--- server/api/openweathermap.js
+++ server/api/openweathermap.js
@@ -3,15 +3,15 @@ export default defineEventHandler(async (event) => {
   const { weatherAPIKey } = useRuntimeConfig();
 
   const units = 'imperial';
-  const locationUri = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${weatherAPIKey}`;
+  const gecodingAPIUrl = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${weatherAPIKey}`;
 
   try {
-    // Use geocode API to get the lat,lon from zip
-    const { name, lat, lon, country } = await $fetch(locationUri);
+    // Use geocode API to get the lat,lon from the zipcode
+    const { name, lat, lon } = await $fetch(gecodingAPIUrl);
 
     // Use openweathermap api to get the forecast
-    const weatherUri = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${weatherAPIKey}`;
-    const data = await $fetch(weatherUri);
+    const weatherAPIUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${weatherAPIKey}`;
+    const data = await $fetch(weatherAPIUrl);
 
     return data;
   } catch (error) {
