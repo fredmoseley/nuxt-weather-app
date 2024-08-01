be1c9107c8b9ddefca43b3f0d0636dda112c7f13
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-31 17:45:09 -0700

    Add the about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,19 @@
 import { ref } from 'vue';
 </script>
 <template>
-  <h1>About.html</h1>
-  <div></div>
+  <div class="text-black card">
+    <h1 class="text-2xl mb-1">About:</h1>
+    <p class="mb-4">The Nuxt Weather App allows you to look up the 3-Hour forecast for the next 5 days for any city in the United States.</p>
+    <h2 class="text-2xl">How it works:</h2>
+    <ol class="list-decimal list-inside mb-4">
+      <li>Enter the 5 digit zip code for the city.</li>
+      <li>Press the Submit button</li>
+    </ol>
+    <p>Use the navigation bar to return to the Home screen and look up another city.</p>
+    <br />
+    <p class="font-bold">Alternatively, you could retrieve the weather forecast by appending  '/weather/[zip]' to the end of the root URL.   For example '/weather/90210'</p>
+
+  </div>
 </template>
 
 <style scoped></style>
