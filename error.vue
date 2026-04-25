588dd73d353105b1673574b094fc6635029b2f0c
fmoseley <fredmoseley@stanfordalumni.org>
2026-04-25 13:21:29 -0700

    Fix lint formatting

--- error.vue
+++ error.vue
@@ -1,14 +1,19 @@
 <script setup>
 defineProps(['error'])
 
-const handleClearError= () => clearError({redirect:'/'})
+const handleClearError = () => clearError({ redirect: '/' })
 </script>
 <template>
   <div class="mt-7 mx-auto text-center card">
-    <p class="mt-7 text-7xl font-bold"> {{ error.statusCode }}</p>
+    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Ooops.</p>
     <p class="mt-7 text-3xl">{{ error.message }}</p>
-    <button class="btn my-7" @click="handleClearError">Go Home</button>
+    <button
+      class="btn my-7"
+      @click="handleClearError"
+    >
+      Go Home
+    </button>
   </div>
 </template>
 
